44e11c95a84320814356155be2b574b5528ac9a3
BuhaDoru <113370353+BuhaDoru@users.noreply.github.com>
2022-10-20 17:58:10 +0300

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -14,42 +14,23 @@
       } 
 
       function playerTurn() {
-        let b1, b2, b3, b4, b5, b6, b7, b8, b9;
-        b1 = document.getElementById("b1").value;
-        b2 = document.getElementById("b2").value;
-        b3 = document.getElementById("b3").value;
-        b4 = document.getElementById("b4").value;
-        b5 = document.getElementById("b5").value;
-        b6 = document.getElementById("b6").value;
-        b7 = document.getElementById("b7").value;
-        b8 = document.getElementById("b8").value;
-        b9 = document.getElementById("b9").value;
-
-        if (((b1 == 'X') && (b2 == 'X') && (b3 == 'X') || ((b1 == 'X') && (b5 == 'X') && (b9 == 'X')) || ((b1 == 'X') && (b4 == 'X') && (b7 == 'X')) || ((b4 == 'X') && (b5 == 'X') && (b6 == 'X')) || ((b2 == 'X') && (b5 == 'X') && (b8 == 'X')) || ((b3 == 'X') && (b6 == 'X') && (b9 == 'X')) || ((b3 == 'X') && (b5 == 'X') && (b7 == 'X')) || ((b7 == "X") && (b8 == 'X') && (b9 == 'X')))) {
+        var b = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9"];
+        for ( var i = 0; i < 9; ++i) {
+          b[i] = document.getElementById(b[i]).value;
+        }
+        if (((b[0] == 'X') && (b[1] == 'X') && (b[2] == 'X') || ((b[0] == 'X') && (b[4] == 'X') && (b[8] == 'X')) || ((b[0] == 'X') && (b[3] == 'X') && (b[6] == 'X')) || ((b[3] == 'X') && (b[4] == 'X') && (b[5] == 'X')) || ((b[1] == 'X') && (b[4] == 'X') && (b[7] == 'X')) || ((b[2] == 'X') && (b[5] == 'X') && (b[8] == 'X')) || ((b[2] == 'X') && (b[4] == 'X') && (b[6] == 'X')) || ((b[6] == "X") && (b[7] == 'X') && (b[8] == 'X')))) {
           document.getElementById('print').innerHTML = "Player X won";
-          document.getElementById("b1").disabled = true;
-          document.getElementById("b2").disabled = true;
-          document.getElementById("b3").disabled = true;
-          document.getElementById("b4").disabled = true;
-          document.getElementById("b5").disabled = true;
-          document.getElementById("b6").disabled = true;
-          document.getElementById("b7").disabled = true;
-          document.getElementById("b8").disabled = true;
-          document.getElementById("b9").disabled = true;
+          for (var j = 1; j <= 9; ++j) {
+            document.getElementById("b" + j).disabled = true;
+          }
         }
-        else if (((b1 == '0') && (b2 == '0') && (b3 == '0') || ((b1 == '0') && (b5 == '0') && (b9 == '0')) || ((b1 == '0') && (b4 == '0') && (b7 == '0')) || ((b4 == '0') && (b5 == '0') && (b6 == '0')) || ((b2 == '0') && (b5 == '0') && (b8 == '0')) || ((b3 == '0') && (b6 == '0') && (b9 == '0')) || ((b3 == '0') && (b5 == '0') && (b7 == '0')) || ((b7 == "0") && (b8 == "0") && (b9 == "0")))) {
+        else if (((b[0] == '0') && (b[1] == '0') && (b[2] == '0') || ((b[0] == '0') && (b[4] == '0') && (b[8] == '0')) || ((b[0] == '0') && (b[3] == '0') && (b[6] == '0')) || ((b[3] == '0') && (b[4] == '0') && (b[5] == '0')) || ((b[3] == '0') && (b[4] == '0') && (b[7] == '0')) || ((b[2] == '0') && (b[5] == '0') && (b[8] == '0')) || ((b[2] == '0') && (b[4] == '0') && (b[6] == '0')) || ((b[6] == "0") && (b[7] == "0") && (b[8] == "0")))) {
           document.getElementById('print').innerHTML = "Player 0 won";
-          document.getElementById("b1").disabled = true;
-          document.getElementById("b2").disabled = true;
-          document.getElementById("b3").disabled = true;
-          document.getElementById("b4").disabled = true;
-          document.getElementById("b5").disabled = true;
-          document.getElementById("b6").disabled = true;
-          document.getElementById("b7").disabled = true;
-          document.getElementById("b8").disabled = true;
-          document.getElementById("b9").disabled = true;
+          for (var j = 1; j <= 9; ++j) {
+            document.getElementById("b" + j).disabled = true;
+          }
         }
-        else if ((b1 == 'X') || (b1 == '0') && (b2 == 'X') || (b2 == '0') && (b3 == 'X') || (b3 == '0') && (b4 == 'X') || (b4 == '0') && (b5 == 'X') || (b5 == '0') && (b6 == 'X') || (b6 == '0') && (b7 == 'X') || (b7 == '0') && (b8 == 'X') || (b8 == '0') && (b9 == 'X') || (b9 == '0')) {
+        else if ((b[0] == 'X') || (b[0] == '0') && (b[1] == 'X') || (b[1] == '0') && (b[2] == 'X') || (b[2] == '0') && (b[3] == 'X') || (b[3] == '0') && (b[4] == 'X') || (b[4] == '0') && (b[5] == 'X') || (b[5] == '0') && (b[6] == 'X') || (b[6] == '0') && (b[7] == 'X') || (b[7] == '0') && (b[8] == 'X') || (b[8] == '0')) {
           document.getElementById('print')
                   .innerHTML = "I'ts a tie";
         }
@@ -65,14 +46,5 @@
 
       function restartGame() {
         location.reload();
-        document.getElementById('b1').value = '';
-        document.getElementById("b2").value = '';
-        document.getElementById("b3").value = '';
-        document.getElementById("b4").value = '';
-        document.getElementById("b5").value = '';
-        document.getElementById("b6").value = '';
-        document.getElementById("b7").value = '';
-        document.getElementById("b8").value = '';
-        document.getElementById("b9").value = '';
       }
     </script>
